183283b317a0c1c05f2066fef2857c070c95d2b6
Antony Ashwin <antonyashwin555@gmail.com>
2025-07-06 01:34:28 +0530

    add thoughts

--- src/Blog.js
+++ src/Blog.js
@@ -5,12 +5,30 @@ export default function Blog() {
     <section className="win95-section">
       <h2>Blog / Thoughts</h2>
       <div className="blog-post">
-        <h3>Why I Love Coding</h3>
-        <p>Some thoughts on coding, gaming, and tech...</p>
-      </div>
-      <div className="blog-post">
-        <h3>Recent Game Review</h3>
-        <p>Review of my favorite game this year...</p>
+        <h3>Euphemism - i recently learnt what this means </h3>
+        <p>A euphemism is a mild or indirect word or expression used in place of something that might be harsh, unpleasant, embarrassing, or blunt.</p>
+        When Euphemism is BAD in Agile: <br></br>
+It hides real problems
+
+Saying “We had a few delays” instead of “We underestimated our capacity by 50%” kills transparency.
+
+It slows feedback loops
+
+Agile relies on fast, honest iteration. Euphemisms delay real conversations.
+
+It dilutes retrospectives
+
+A retro full of “some communication gaps” instead of “the design was never explained” leads to no improvement.
+
+It creates false alignment
+
+Everyone nods, but no one really knows what went wrong.
+
+Idk just my thoughts. <br></br><br></br>
+
+Maybe i should plug this to an sql server and make an actual blog out of it where others can reply. <br></br>
+
+
       </div>
     </section>
   );
