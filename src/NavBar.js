14b6962fce6999184354dd2cecaebd30331ecad7
Antony Ashwin <antonyashwin555@gmail.com>
2025-07-06 00:43:05 +0530

    add personal projects

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,8 +5,8 @@ export default function NavBar({ onNav }) {
     <nav className="nav-bar">
       <button className="nav-link" onClick={() => onNav('home')}>Home</button>
       <button className="nav-link" onClick={() => onNav('about')}>About Me</button>
-      <button className="nav-link" onClick={() => onNav('projects')}>Projects</button>
-      <button className="nav-link" onClick={() => onNav('resume')}>Resume</button>
+      <button className="nav-link" onClick={() => onNav('projects')}>Academic Projects</button>
+      <button className="nav-link" onClick={() => onNav('resume')}>Personal Projects</button>
       <button className="nav-link" onClick={() => onNav('blog')}>Blog</button>
       <button className="nav-link" onClick={() => onNav('hobbies')}>Hobbies</button>
       <button className="nav-link" onClick={() => onNav('contact')}>Contact</button>
