cede84287f754b25703862a1414b42216a237f20
Antony Ashwin <antonyashwin555@gmail.com>
2025-07-06 01:07:14 +0530

    update personal projects

--- src/PersonalProjects.js
+++ src/PersonalProjects.js
@@ -3,13 +3,31 @@ import React from 'react';
 export default function PersonalProjects() {
   return (
     <section className="win95-section">
-      <h2>Academic Projects</h2>
+
+        <h2>Personal Projects</h2>
       <div className="project-card">
-        <h3>B.tech 5th Semester</h3>
-        <p>Fuel-Management-System - A C# application to manage consumption and reception of fuel</p>
-        <a href="https://github.com/AntonyAshwin/Fuel-Management-System/tree/main/FMS">Github</a> <br></br>
-        <a href="https://github.com/AntonyAshwin/Fuel-Management-System/blob/main/dbms%20project%20report.pdf" download>Report</a>
+        <p>Ashwin's App Generator - An App which generates an app based on user prompt</p>
+        <a href="https://github.com/AntonyAshwin/ashwin">Github</a> <br></br>
       </div>
+              <div className="project-card">
+        <p>Versine Calculator - A static web app  to calculate the chord versine - can be used offline too
+            - made this for my dad, he loved it :)
+        </p>
+        <a href="https://github.com/AntonyAshwin/chordversine_calculator_KRIDE">Github</a> <br></br>
+      </div>
+      <div className="project-card">
+        <p>Morse Activator - a python script which translates texts to CW waves - transmits from output speakers</p>
+        <a href="https://github.com/AntonyAshwin/MorseCode-python">Github</a> <br></br>
+      </div>
+
+          <div className="project-card">
+        <p>Ashwin's Music Bot - Discord Bot for playing music, Discord was banning mainstream
+            bots. i had to step in to fill the gap.
+        </p>
+        <a href="https://github.com/AntonyAshwin/MorseCode-python">Github</a> <br></br>
+      </div>
+
+
     </section>
   );
 }
