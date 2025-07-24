0a932b2384bc523363a6c861c0b5060657f52cf0
Antony Ashwin <antonyashwin555@gmail.com>
2025-07-24 22:55:57 +0530

    fix order of academic projects

--- src/Projects.js
+++ src/Projects.js
@@ -4,18 +4,42 @@ export default function Projects() {
   return (
     <section className="win95-section">
       <h2>Academic Projects</h2>
-      <div className="project-card">
-        <h3>B.tech 5th Semester</h3>
-        <p>Fuel-Management-System - A C# application to manage consumption and reception of fuel</p>
-        <a href="https://github.com/AntonyAshwin/Fuel-Management-System/tree/main/FMS">Github</a> <br></br>
-        <a href="https://github.com/AntonyAshwin/Fuel-Management-System/blob/main/dbms%20project%20report.pdf" download>Report</a>
+
+        <div className="project-card">
+        <h3>M.tech 2nd year</h3>
+        <p>Instrument Code Synchronization - I made a React application that provides a form for submitting and synchronizing instrument codes acorss different landscapes.</p>
+        <a href="https://github.com/AntonyAshwin/FinalSemProject">Github</a> <br />
+        <p>Note : This project was done in close collab with the org i work in. lot of 
+            closed source code. please ping me for the access to this repo and the report.
+        </p>
       </div>
+
       <div className="project-card">
-        <h3>B.tech 6th Semester</h3>
-        <p>Ashwin's 2D game - A 2D game in C using openGL.</p>
-        <a href="https://github.com/AntonyAshwin/openGL_mini_project">Github</a> <br></br>
-        <p>Note : clone and run it by hitting cc 1.c -lGL -lGLU -lglut -lm on your terminal</p>
+        <h3>M.tech 2nd year</h3>
+        <p>Game Achievement Management System - The motive of this project was to demonstrate the advantages of using Microservices.
+            I have different microservices in different languages.
+        </p>
+        <a href="https://github.com/AntonyAshwin/ScalableServiceAssignment">Github</a> <br />
+        <p>Note : Worked on it all night - I remember Abhishek also working on something similar.
+        </p>
       </div>
+
+    
+
+      <div className="project-card">
+        <h3>M.tech 1st year</h3>
+        <p>Gold Provenance using BlockChain - We made a decentralized application to track the origin and ownership of gold</p>
+        <a href="https://github.com/AntonyAshwin/Project-SESAPZG-504-569">Github</a> <br />
+        <p>Note : Again, I have a hard copy of the Report - might upload it here later</p>
+      </div>
+
+      <div className="project-card">
+        <h3>B.tech 8th Semester</h3>
+        <p>Land Regisry using BlockChain - We made a decentralized applciation to ease the transfer of ownership of land</p>
+        <a href="https://github.com/AntonyAshwin/Final-Year-Project-2023">Github</a> <br />
+        <p>Note : I have a hard copy of the Report - might upload it here later</p>
+      </div>
+
       <div className="project-card">
         <h3>B.tech 7th Semester</h3>
         <p>Scheduler Application - We made an Android application to schedule meetings - something like teams</p>
@@ -25,36 +49,17 @@ export default function Projects() {
       </div>
 
       <div className="project-card">
-        <h3>B.tech 8th Semester</h3>
-        <p>Land Regisry using BlockChain - We made a decentralized applciation to ease the transfer of ownership of land</p>
-          <a href="https://github.com/AntonyAshwin/Final-Year-Project-2023">Github</a> <br></br>
-        <p>Note : I have a hard copy of the Report - might upload it here later</p>
-      </div>
-
-        <div className="project-card">
-        <h3>M.tech 1st year</h3>
-        <p>Gold Provenance using BlockChain - We made a decentralized application to track the origin and ownership of gold</p>
-          <a href="https://github.com/AntonyAshwin/Project-SESAPZG-504-569">Github</a> <br></br>
-        <p>Note : Again, I have a hard copy of the Report - might upload it here later</p>
+        <h3>B.tech 6th Semester</h3>
+        <p>Ashwin's 2D game - A 2D game in C using openGL.</p>
+        <a href="https://github.com/AntonyAshwin/openGL_mini_project">Github</a> <br />
+        <p>Note : clone and run it by hitting cc 1.c -lGL -lGLU -lglut -lm on your terminal</p>
       </div>
 
       <div className="project-card">
-        <h3>M.tech 2nd year</h3>
-        <p>Game Achievement Management System - The motive of this project was to demonstrate the advantages of using Microservices.
-            I have different microservices in different languages.
-        </p>
-          <a href="https://github.com/AntonyAshwin/ScalableServiceAssignment">Github</a> <br></br>
-        <p>Note : Worked on it all night - I remember Abhishek also working on something similar.
-        </p>
-      </div>
-
-      <div className="project-card">
-        <h3>M.tech 2nd year</h3>
-        <p>Instrument Code Synchronization - I made a React application that provides a form for submitting and synchronizing instrument codes acorss different landscapes.</p>
-          <a href="https://github.com/AntonyAshwin/FinalSemProject">Github</a> <br></br>
-        <p>Note : This project was done in close collab with the org i work in. lot of 
-            closed source code. please ping me for the access to this repo and the report.
-        </p>
+        <h3>B.tech 5th Semester</h3>
+        <p>Fuel-Management-System - A C# application to manage consumption and reception of fuel</p>
+        <a href="https://github.com/AntonyAshwin/Fuel-Management-System/tree/main/FMS">Github</a> <br />
+        <a href="https://github.com/AntonyAshwin/Fuel-Management-System/blob/main/dbms%20project%20report.pdf" download>Report</a>
       </div>
     </section>
   );
