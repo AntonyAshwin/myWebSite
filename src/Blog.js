50321129a8317d199ccbaec7c8a32d1ea604bcde
Antony Ashwin <antonyashwin555@gmail.com>
2025-09-10 23:54:20 +0530

    asf

--- src/Blog.js
+++ src/Blog.js
@@ -17,25 +17,6 @@ const posts = [
     )
   },
   {
-    title: "Euphemism - I recently learnt what this means",
-    content: (
-      <>
-        <p>A euphemism is a mild or indirect word or expression used in place of something that might be harsh, unpleasant, embarrassing, or blunt.</p>
-        <b>When Euphemism is BAD in Agile:</b> <br />
-        It hides real problems<br /><br />
-        Saying “We had a few delays” instead of “We underestimated our capacity by 50%” kills transparency.<br /><br />
-        It slows feedback loops<br /><br />
-        Agile relies on fast, honest iteration. Euphemisms delay real conversations.<br /><br />
-        It dilutes retrospectives<br /><br />
-        A retro full of “some communication gaps” instead of “the design was never explained” leads to no improvement.<br /><br />
-        It creates false alignment<br /><br />
-        Everyone nods, but no one really knows what went wrong.<br /><br />
-        Idk just my thoughts.<br /><br />
-        Maybe I should plug this to an SQL server and make an actual blog out of it where others can reply.<br />
-      </>
-    )
-  },
-  {
     title: "Death Stranding review",
     content: (
       <>
